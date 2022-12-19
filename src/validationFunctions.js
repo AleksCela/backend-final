9739966f9e06934d396910c06635752bde18734b
Aleks Cela <alekscela7@gmail.com>
2022-12-19 15:00:22 +0100

    Added signup api/fixed verificationFunctions

--- src/validationFunctions.js
+++ src/validationFunctions.js
@@ -1,20 +1,20 @@
 import database from "./databaseConnectivity.js";
 
-async function CredentialsValidation(username,password){
-    const username_exists = await database.raw(`select * from users where username='${username}'`);
+async function credentialsValidation(email, password) {
+    const email_exists = await database.raw(`select * from users where email='${email}'`);
     const reg_expression = /(?=.*[!?.:])(?=.*\d)/;
-    const username_validation = username.length > 5 && username.length < 12 && username_exists.length == 0;
-    const password_validation = password.length > 5 || password.length < 12 && reg_expression.test(password);
-    
-    if(!username_validation){
-        throw new Error("The username is not valid!");
+    const email_validation = email.length > 5 && email.length < 20 && email_exists.length == 0;
+    const password_validation = password.length > 5 || password.length < 20 && reg_expression.test(password);
+
+    if (!email_validation) {
+        throw new Error("The email is not valid!");
     }
-    else if(!password_validation) {
+    else if (!password_validation) {
         throw new Error("The password is not valid!");
     }
-    else{
+    else {
         return true;
     }
 }
 
-export default CredentialsValidation;
+export default credentialsValidation;
